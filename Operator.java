e5eb62d18fa882b8c903d7115da142ab0830c79a
KIRUBHANANDHAN <viranandhu@gmail.com>
2018-05-29 02:53:39 -0700

    Operator

---
+++ Operator.java
@@ -0,0 +1,13 @@
+class Operator
+{
+public static void main(String args[])
+{
+int a=16,b=12;
+System.out.println("addition is "+(a+b));
+System.out.println("substaction is "+(a-b));
+System.out.println("multiple is "+(a*b));
+System.out.println("inc is "+(++a-b--));
+System.out.println("shift left is "+(a<<1));
+System.out.println("shift right is "+(b>>1));
+}
+}
